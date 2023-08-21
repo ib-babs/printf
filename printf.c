820770b85e8fdedce4360c8b43953c5b2c7aacce
Ibrahim <babatundeibrahim426@gmail.com>
2023-08-21 09:47:53 -0700

    Newest Commit

--- printf.c
+++ printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
 	while (format[i] != '\0')
