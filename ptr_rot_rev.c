f46dd6ff3373675b8aaa58e1e501a6c08e29c6d5
ibrahim <babatundeibrahim426@gmail.com>
2023-08-23 16:32:29 +0100

    Pointer function modified

--- ptr_rot_rev.c
+++ ptr_rot_rev.c
@@ -7,11 +7,11 @@
 int printf_ptr(va_list val)
 {
 	void *ptr = va_arg(val, void *);
-	unsigned int *p;
+	int *p;
 	int len;
 	char *s;
 
-	p = (unsigned int *)&ptr;
+	p = (int *)&ptr;
 	len = digit_len(*p, 16);
 	s = handle_hex_HEX(*p, 87, len);
 
